0caf7a1849b3b0ec99a7297c359f2903dcf782df
Matt Styles <matt@personalurban.com>
2016-08-23 21:02:55 +0100

    initial commit :tada:

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+import Inferno from 'inferno'
+import InfernoDOM from 'inferno-dom'
+
+const App = props => <h1>It’s Dark</h1>
+
+InfernoDOM.render(
+  <App />,
+  document.querySelector('.js-main')
+)
