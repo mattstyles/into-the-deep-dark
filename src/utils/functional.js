b9b03a96eeebacea1d7f1692a331d78752eb07b4
Matt Styles <matt@personalurban.com>
2016-09-20 07:24:23 +0100

    update functional utilities

--- src/utils/functional.js
+++ src/utils/functional.js
@@ -1,7 +1,52 @@
 
+/**
+ * A fold function for iterator values
+ * @returns <any> the result of folding the iterator values
+ * @param it <Map> passes values to the supplied reducer
+ * @param fn <Function> scan/reduce/fold, the callback working on the list
+ * @param initial <any> _optional_ if omitted null will be passed to the first
+ * callback
+ */
 export const iteratorFold = (it, fn, initial) => {
   for (const value of it) {
     initial = fn(initial, value)
   }
   return initial
+}
+
+/**
+ * Returns a new array containing the top `n` items of the array
+ * @returns <Array<any>>
+ * @param num <Integer> _optional_ number of items to return
+ * @param stack <Array<any>>
+ */
+export const head = (num, stack) => {
+  if (typeof num !== 'number' || num <= 1) {
+    return stack[0]
+  }
+
+  if (num >= stack.length) {
+    return stack
+  }
+
+  return stack.slice(0, num)
+}
+
+/**
+ * Returns a new array containing the last `n` items of the array
+ * @returns <Array<any>>
+ * @param num <Integer> _optional_ number of items to return
+ * @param stack <Array<any>>
+ */
+export const tail = (num, stack) => {
+  let last = stack.length - 1
+  if (typeof num !== 'number' || num <= 1) {
+    return stack[last]
+  }
+
+  if (num >= stack.length) {
+    return stack
+  }
+
+  return stack.slice(last - num, last)
 }
